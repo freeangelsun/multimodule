bdc24112045560ea6eaa606d81b4139b21502064
freeangelsun <freeangelsun@gmail.com>
2023-04-26 13:50:24 +0900

    테스트중

--- api/src/main/java/com/multimodule/api/controller/HelloController.java
+++ api/src/main/java/com/multimodule/api/controller/HelloController.java
@@ -1,8 +1,10 @@
 package com.multimodule.api.controller;
 
+import com.multimodule.api.annotation.GlobId;
 import com.multimodule.api.dto.MemberDto;
 import com.multimodule.api.dto.MemberResponse;
 import com.multimodule.common.dao.member.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 public class HelloController {
     @GetMapping("/hello")
@@ -19,14 +22,18 @@ public class HelloController {
     }
 
     @PostMapping(value = "/{version}/test/member")
-    public ResponseEntity<MemberDto.MemberResponse> getMember(@PathVariable("version") String version,
-                                                    @Validated @RequestBody MemberDto.MemberRequest request,
-                                                    BindingResult bindingResult,
-                                                    @RequestHeader  Map<String, String> requestHeader){
+    public ResponseEntity<MemberDto.MemberResponse> getMember(@GlobId String globId,
+                                                              @PathVariable("version") String version,
+                                                              @Validated @RequestBody MemberDto.MemberRequest request,
+                                                              BindingResult bindingResult,
+                                                              @RequestHeader  Map<String, String> requestHeader){
+
 
 
         MemberDto.MemberResponse  result = new MemberDto.MemberResponse("정찬길", "10");
         result.setReqId(request.getReqId());
+
+        log.debug("debug : #####################################################################################################");
 
         if (bindingResult.hasErrors()) {
             result.setRspCode("01");
